83fc2ff025a24a7f46c6339f84aa39e88be17a01
Kasun Lakshitha <kasun2014@outlook.com>
2025-03-17 23:57:54 +0530

    Update index.tsx

--- solid-js-app/src/pages/Home/index.tsx
+++ solid-js-app/src/pages/Home/index.tsx
@@ -4,7 +4,8 @@ const Home = () => {
       <h1 class="text-3xl font-medium text-center">
         Welcome to Naruto's Universe
       </h1>
-
+      {/* TODO: Implement the home page content here.
+      Use https://api-dattebayo.vercel.app/docs API */}
       <div class="grid grid-cols-3 gap-4 pt-8">
         <div class="bg-gray-200 hover:bg-gray-400 p-4 rounded text-center">
           <a href="/characters">Characters</a>
