761e49c7de8accd588152296e9beff3a10b1f5bc
Kasun Lakshitha <kasun2014@outlook.com>
2025-04-14 23:53:21 +0530

    Add @tailwindcss/line-clamp and improve blog post styling

--- ts-router-app/src/routes/index.tsx
+++ ts-router-app/src/routes/index.tsx
@@ -16,13 +16,18 @@ function App() {
   const posts = ImportedPostData as Array<PostDataProps>
   return (
     <main className="p-8">
-      <div className="container mx">
+      <div className="container max-w-5xl mx-auto">
         <div className="text-center text-4xl">DevKasun Blog</div>
 
         {posts.map((post) => (
-          <div key={post.id} className="my-4">
+          <div
+            key={post.id}
+            className="my-4 p-4 border-2 border-solid border-cyan-600 rounded-md hover:shadow-md hover:bg-cyan-600 group"
+          >
             <h2 className="text-2xl">{post.title}</h2>
-            <p className="text-gray-600">{post.content}</p>
+            <p className="text-gray-700 group-hover:text-black line-clamp-1">
+              {post.content}
+            </p>
           </div>
         ))}
       </div>
